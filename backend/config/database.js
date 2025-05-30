27d378ac58a7bb258193ea91ccb953b56d0ef35e
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-05-30 20:32:39 +0300

    up

---
+++ backend/config/database.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+
+const database = () => {
+try {
+    mongoose.connect(process.env.MONGO_URI).then(() => {
+        console.log("mongoDB connected")
+    }).catch((err) => {
+        console.log(err)
+    });
+} catch (error) {
+    console.log(error)
+}
+}
+
+module.exports = database
--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
+const database = require("./config/database.js");
 
 dotenv.config()
 
@@ -11,6 +12,8 @@ app.use(bodyParser.json({limit:'30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
 
 const PORT = process.env.PORT || 8000;
+
+database()
 
 app.listen(PORT, () => {
     console.log("server is running", PORT)
