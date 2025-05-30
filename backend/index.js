29c51836b0a0c58522889ab9b2fdd0f719505f79
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-05-30 19:51:35 +0300

    up

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,17 @@
 const express = require("express");
 const cors = require("cors");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 const app = express();
+app.use(cors())
+app.use(bodyParser.json({limit:'30mb', extended: true}))
+app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
 
 const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log("server is running", PORT)
+})
