291bc4f101bb3a96dab3a7a3ebb78ccb7a5e0012
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-05-30 20:11:18 +0300

    up

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(bodyParser.json({limit:'30mb', extended: true}))
 app.use(bodyParser.urlencoded({limit:'30mb', extended: true}))
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log("server is running", PORT)
