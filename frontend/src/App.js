bfedeba1e1f9d2cb2d90fd366641482ea8f9e4fd
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-01 18:18:48 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
+import Home from './pages/home'
+import Auth from './pages/auth'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          {/* <Route path="/auth" element={<Auth />} /> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </BrowserRouter>
     </div>
