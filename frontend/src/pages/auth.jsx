58536a6ece4f299b4732acd1891bd4eb274e5636
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-01 19:01:24 +0300

    up

--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const auth = () => {
+const Auth = () => {
+    const [signUp, setSignUp] = useState(true)
   return (
     <div className='w-full h-screen bg-gray-200 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50' >
         <div className='w-1/3 bg-white p-3'>
-            <h1>BLOG TIME</h1>
-            <div className='fşex fşex-col space-y-4 my-4' >
-                <input type="text" placeholder='Username' className='input-style' />
+            <h1 className='text-2xl text-lime-500 font-bold'>{signUp ? "KAYIT OL" : "GİRİŞ YAP"}</h1>
+            <div className='flex flex-col space-y-4 my-4' >
+                {signUp && <input type="text" placeholder='Username' className='input-style' />}
                 <input type="text" placeholder='Email' className='input-style' />
                 <input type="text" placeholder='Password' className='input-style' />
             </div>
-            <div className='cursor-pointer hover:bg-lime-600 w-full p-3 text-center text-white rounded-md bg-lime-400' >Kayıt Ol</div>
+            <div className='text-red-400 text-xs cursor-pointer mb-3' >
+                {
+                    signUp ? <span onClick={()=>setSignUp(false)}>Daha önce giriş yaptınız mı?</span> : <span onClick={()=>setSignUp(true)}>Kayıt olmak için tıklayınız</span>
+                }
+                
+            </div>
+            <div className='cursor-pointer hover:bg-lime-600 w-full p-3 text-center text-white rounded-md bg-lime-400' >{signUp ? "Kayıt Ol" : "Giriş Yap"}</div>
         </div>
     </div>
   )
 }
 
-export default auth
+export default Auth
