b24f25774bbbd7719b4882f77ea5babf963863c1
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-01 17:58:00 +0300

    up

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,13 +1,31 @@
-import { applyMiddleware, combineReducers, createStore } from "redux"
+import { combineReducers,} from "redux";
+import { configureStore } from '@reduxjs/toolkit'
 
-const initialState = {
+const initialState = {};
 
-}
+const rootReducer = combineReducers({});
 
-const reducers = combineReducers({
-
-})
-
-const store = createStore(reducers, initialState, composeWithDevTools(applyMiddleware(thunk)))
+const store = configureStore({
+  reducer: rootReducer,
+  proloadedState: initialState,
+});
 
 export default store;
+
+
+
+// import { applyMiddleware, combineReducers, createStore } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+// import { thunk } from "redux-thunk";
+
+// const initialState = {};
+
+// const reducers = combineReducers({});
+
+// const store = createStore(
+//   reducers,
+//   initialState,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+// export default store;
