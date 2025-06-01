981887914fb925d1df169b592d3151eec5f89b7d
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-01 18:39:29 +0300

    commit

--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -2,7 +2,17 @@ import React from 'react'
 
 const auth = () => {
   return (
-    <div>auth</div>
+    <div className='w-full h-screen bg-gray-200 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50' >
+        <div className='w-1/3 bg-white p-3'>
+            <h1>BLOG TIME</h1>
+            <div className='fşex fşex-col space-y-4 my-4' >
+                <input type="text" placeholder='Username' className='input-style' />
+                <input type="text" placeholder='Email' className='input-style' />
+                <input type="text" placeholder='Password' className='input-style' />
+            </div>
+            <div className='cursor-pointer hover:bg-lime-600 w-full p-3 text-center text-white rounded-md bg-lime-400' >Kayıt Ol</div>
+        </div>
+    </div>
   )
 }
 
