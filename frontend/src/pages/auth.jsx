8c4edf6bb0612ed89883a218e76b9e923dad015f
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-04 20:03:15 +0300

    up

--- frontend/src/pages/auth.jsx
+++ frontend/src/pages/auth.jsx
@@ -3,14 +3,22 @@ import React, { useState } from 'react'
 
 const Auth = () => {
     const [signUp, setSignUp] = useState(true)
+    const [authData, setAuthData] = useState({username: "", email: "", password:""})
+
+    const onChangeFunc = (e) => {
+        setAuthData({...authData, [e.target.name] : e.target.value})
+    }
+
+    console.log("authData", authData);
+
   return (
     <div className='w-full h-screen bg-gray-200 flex items-center justify-center fixed top-0 right-0 bottom-0 left-0 z-50' >
         <div className='w-1/3 bg-white p-3'>
             <h1 className='text-2xl text-lime-500 font-bold'>{signUp ? "KAYIT OL" : "GİRİŞ YAP"}</h1>
             <div className='flex flex-col space-y-4 my-4' >
-                {signUp && <input type="text" placeholder='Username' className='input-style' />}
-                <input type="text" placeholder='Email' className='input-style' />
-                <input type="text" placeholder='Password' className='input-style' />
+                {signUp && <input value={authData.username} name='username' onChange={onChangeFunc} type="text" placeholder='Username'  className='input-style' />}
+                <input value={authData.email} name='email' onChange={onChangeFunc} type="text" placeholder='Email' className='input-style' />
+                <input value={authData.password} name='passwprd' onChange={onChangeFunc} type="text" placeholder='Password' className='input-style' />
             </div>
             <div className='text-red-400 text-xs cursor-pointer mb-3' >
                 {
