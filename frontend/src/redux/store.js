53dbc5914155141a23fe99cc67986be1424fea92
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-05 20:42:53 +0300

    up

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,12 @@
 import { combineReducers } from "redux";
 import { configureStore } from '@reduxjs/toolkit'
+import authReducer from "./reducers/auth";
 
 const initialState = {};
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  auth: authReducer
+});
 
 const store = configureStore({
   reducer: rootReducer,
