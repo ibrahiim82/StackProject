46fe324337f965abce628b49db303ad06c94b2de
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-16 16:01:43 +0300

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Routes, Route, Link} from 'react-router-dom'
 import Home from './pages/home'
 import Auth from './pages/auth'
 import { ToastContainer} from 'react-toastify';
+import useToken from './hooks/useToken';
 
 function App() {
+  const [token] = useToken()
+
+  console.log(token.token, "token");
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={!token.token ? <Link to={'/auth'} /> : <Home />} />
           <Route path="/auth" element={<Auth />} />
         </Routes>
       </BrowserRouter>
