85736bcf90457eca6ec87f9a28edc7967488b5c0
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-16 16:50:11 +0300

    up

--- frontend/src/components/Modal.jsx
+++ frontend/src/components/Modal.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import { useState } from "react"
+
 
 const Modal = () => {
+    const [postData, setPostData] = useState({
+        user:"",
+        title:"",
+        description:""
+    })
+    const onChangeFunc = (e) => {
+        setPostData({...postData, [e.target.name] : e.target.value})
+    }
   return (
     <div className='w-full h-screen bg-opacity-50 bg-black fixed inset-0 z-50 flex items-center justify-center'>
         <div className='bg-white w-1/3 p-2 rounded-md'>
-        <h1>YORUM YAZ</h1>
-            <div>
-                <input type="text" placeholder='User' />
-                <input type="text" placeholder='Title' />
-                <input type="text" placeholder='Description' />
+        <h1 className='font-bold text-2xl'>YORUM YAZ</h1>
+            <div className='my-4 flex-col space-y-3'>
+                <input value={postData.user} name="user" onChange={onChangeFunc} className='input-style' type="text" placeholder='User' />
+                <input value={postData.title} name="title" onChange={onChangeFunc} className='input-style' type="text" placeholder='Title' />
+                <input value={postData.description} name="description" onChange={onChangeFunc} className='input-style' type="text" placeholder='Description' />
             </div>
-            <div>Paylaş</div>
+            <div className='w-full p-2 text-center bg-lime-500 text-white cursor-pointer hover:bg-lime-600'>Paylaş</div>
         </div>
     </div>
   )
