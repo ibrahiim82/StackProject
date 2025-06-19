9b7bfd59ac5a5e486f1216ae8519a492d12a282a
ibrahiim82 <yavuzyolcu82@gmail.com>
2025-06-19 19:40:31 +0300

    commit

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,19 +1,25 @@
-import { useSelector } from 'react-redux'
-import HomeCard from '../components/HomeCard';
-
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { getPostsAction } from '../redux/actions/post'
+import HomeCard from '../components/HomeCard'
 
 const Home = () => {
+  const dispatch = useDispatch()
+  const { posts } = useSelector(state => state.posts)
 
-  const {posts} = useSelector(state => state.posts)
+  useEffect(() => {
+    dispatch(getPostsAction())
+  }, [dispatch])
 
-  console.log("Posts:", posts);
+  console.log("Posts:", posts)
+
   return (
     <div className='flex items-center m-5 flex-wrap gap-6 pl-16'>
-       {
-        posts?.map((post,i) => (
+      {
+        posts?.map((post, i) => (
           <HomeCard key={i} post={post} />
         ))
-       }
+      }
     </div>
   )
 }
